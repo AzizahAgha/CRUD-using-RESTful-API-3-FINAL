e55c5b3587e4867879e9b3f1e52007d7d521dad4
Azizah Agha <117432933+AzizahAgha@users.noreply.github.com>
2023-01-05 17:16:04 +0300

    Update ListPost.js

--- src/components/ListPost.js
+++ src/components/ListPost.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate,useParams } from 'react-router-dom';
-import MaterialTable from "material-table";
 import { useTable } from 'react-table'
 
 export default function ListPost(){
